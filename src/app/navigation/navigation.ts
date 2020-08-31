cc884e159e82cae4bfab967950f97598a7d8337a
Noor14 <nooraliyahoo95@gmail.com>
2020-08-31 15:41:35 +0500

    changes

--- src/app/navigation/navigation.ts
+++ src/app/navigation/navigation.ts
@@ -9,7 +9,56 @@ export const navigation: FuseNavigation[] = [
                 url      : '/sample'
             },
             {
-                id       : 'sample',
+                id       : 'partner',
+                title    : 'Partners',
+                type     : 'item',
+                icon     : 'local_parking',
+                url      : '/samplsde'
+            },
+            {
+                id       : 'reseller',
+                title    : 'Resellers',
+                type     : 'item',
+                icon     : 'device_hub',
+                url      : '/sampldse'
+            },
+               {
+                id       : 'merchant',
+                title    : 'Merchants',
+                type     : 'item',
+                icon     : 'home',
+                url      : '/sampldde'
+            },
+            {
+                id       : 'pricing',
+                title    : 'Pricing Plan',
+                type     : 'item',
+                icon     : 'event_note',
+                url      : '/samplde'
+            },
+            {
+                id       : 'sale',
+                title    : 'Make a Sale',
+                type     : 'item',
+                icon     : 'account_balance_wallet',
+                url      : '/samples'
+            },
+            {
+                id       : 'transaction',
+                title    : 'Transaction',
+                type     : 'item',
+                icon     : 'attach_money',
+                url      : '/samples'
+            },
+            {
+                id       : 'fund',
+                title    : 'Funding Manager',
+                type     : 'item',
+                icon     : 'account_balance',
+                url      : '/samples'
+            },
+            {
+                id       : 'user_mgmt',
                 title    : 'User Managment',
                 type     : 'collapsable',
                 icon     : 'supervised_user_circle',
@@ -24,59 +73,11 @@ export const navigation: FuseNavigation[] = [
                ]
             },
             {
-                id       : 'sample',
-                title    : 'Merchants',
-                type     : 'item',
-                icon     : 'people',
-                url      : '/sampldde'
-            },
-            {
-                id       : 'sample',
-                title    : 'Partners',
-                type     : 'item',
-                icon     : 'attachment',
-                url      : '/samplsde'
-            },
-            {
-                id       : 'sample',
-                title    : 'Resellers',
-                type     : 'item',
-                icon     : 'euro_symbol',
-                url      : '/sampldse'
-            },
-            {
-                id       : 'sample',
-                title    : 'Pricing Plan',
-                type     : 'item',
-                icon     : 'event_note',
-                url      : '/samplde'
-            },
-            {
-                id       : 'sample',
-                title    : 'Transaction',
-                type     : 'item',
-                icon     : 'attach_money',
-                url      : '/samples'
-            },
-            {
-                id       : 'sample',
-                title    : 'Funding Manager',
-                type     : 'item',
-                icon     : 'account_balance',
-                url      : '/samples'
-            },
-            {
-                id       : 'sample',
+                id       : 'setting',
                 title    : 'Settings',
                 type     : 'item',
                 icon     : 'settings',
                 url      : '/samples'
-            },
-            {
-                id       : 'sample',
-                title    : 'Make a Sale',
-                type     : 'item',
-                icon     : 'account_balance_wallet',
-                url      : '/samples'
             }
+            
 ];
