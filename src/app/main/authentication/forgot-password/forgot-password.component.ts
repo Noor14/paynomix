70a3a95b0c16bdead2caaeb7127edac6aa45bf13
Noor14 <nooraliyahoo95@gmail.com>
2020-09-09 16:23:28 +0500

    forgot password

--- src/app/main/authentication/forgot-password/forgot-password.component.ts
+++ src/app/main/authentication/forgot-password/forgot-password.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { FuseConfigService } from '@fuse/services/config.service';
+import { fuseAnimations } from '@fuse/animations';
+import { environment } from 'environments/environment';
 
 @Component({
-  selector: 'app-forgot-password',
-  templateUrl: './forgot-password.component.html',
-  styleUrls: ['./forgot-password.component.scss']
+    selector     : 'forgot-password',
+    templateUrl  : './forgot-password.component.html',
+    styleUrls    : ['./forgot-password.component.scss'],
+    encapsulation: ViewEncapsulation.None,
+    animations   : fuseAnimations
 })
-export class ForgotPasswordComponent implements OnInit {
+export class ForgotPasswordComponent implements OnInit
+{
+    public forgotPasswordForm: FormGroup;
+    public appInfo = environment;
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+    /**
+     * Constructor
+     *
+     * @param {FormBuilder} _formBuilder
+     */
+    constructor(
+        private _formBuilder: FormBuilder
+    )
+    {}
 
+    // -----------------------------------------------------------------------------------------------------
+    // @ Lifecycle hooks
+    // -----------------------------------------------------------------------------------------------------
+
+    /**
+     * On init
+     */
+    ngOnInit(): void
+    {
+        this.forgotPasswordForm = this._formBuilder.group({
+            email: ['', [Validators.required, Validators.email]]
+        });
+    }
 }
