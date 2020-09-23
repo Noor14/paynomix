6f2b075f7f1764a3d156991826e9929f90e391a9
Noor14 <nooraliyahoo95@gmail.com>
2020-09-23 19:51:44 +0500

    done page on merchant boarding

--- src/app/main/pages/merchant/merchant-form/merchant-form.component.ts
+++ src/app/main/pages/merchant/merchant-form/merchant-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ChangeDetectorRef, Input, Output, EventEmitter } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { MatStepper } from '@angular/material/stepper';
 
 @Component({
   selector: 'app-merchant-form',
@@ -14,22 +15,33 @@ export class MerchantFormComponent implements OnInit {
   public bankAccountForm: FormGroup;
   @Input() merchantDetail: any = null;
   @Output() submitForm = new EventEmitter<any>();
+  public boardingObject:any; 
 
 
   constructor(private readonly _cdref: ChangeDetectorRef) { }
 
   ngOnInit(): void {
     this._cdref.detectChanges();
+  
   }
 
-  save(): void{
-    const obj = {
+  private createBoardingObject(): void{
+    this.boardingObject = {
       ...{...this.merchantDetail, ...this.ownerDetailForm.value},
       MerchantBankAccount: (this.merchantDetail && this.merchantDetail.MerchantBankAccount)? {...this.merchantDetail.MerchantBankAccount, ...this.bankAccountForm.value} : {...this.bankAccountForm.value},
       MerchantAccountSetup: (this.merchantDetail && this.merchantDetail.MerchantAccountSetup)? {...this.merchantDetail.MerchantAccountSetup, ...this.merchantInfoForm.value}: {...this.merchantInfoForm.value},
       MerchantBusiness: (this.merchantDetail && this.merchantDetail.MerchantBusiness)? {...this.merchantDetail.MerchantBusiness, ...this.businessDetailForm.value}: {...this.businessDetailForm.value},
     }
-    this.submitForm.emit(obj);
+  }
+  stepChange(event: MatStepper): void{
+    if(event.selectedIndex == 4){
+      this.createBoardingObject();
+    }
+  }
+
+  save(): void{
+    this.createBoardingObject();
+    this.submitForm.emit(this.boardingObject);
   }
 
 }
