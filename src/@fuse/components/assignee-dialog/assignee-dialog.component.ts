fea1ef8328b5102491e5af815fdfe9fe7b3cd4d8
Noor14 <nooraliyahoo95@gmail.com>
2020-09-28 16:27:43 +0500

    changes

---
+++ src/@fuse/components/assignee-dialog/assignee-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-assignee-dialog',
+  templateUrl: './assignee-dialog.component.html',
+  styleUrls: ['./assignee-dialog.component.scss']
+})
+export class AssigneeDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
