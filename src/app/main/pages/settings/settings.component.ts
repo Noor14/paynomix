b6ff75691fe448c228ddfe75ca6d9f004e7ed104
muhammadsami985 <muhammadsami985@gmail.com>
2020-09-30 19:56:42 +0500

    setting form changes

--- src/app/main/pages/settings/settings.component.ts
+++ src/app/main/pages/settings/settings.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-
+import * as globalConfig from '../../../../constants/globalFunctions';
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
@@ -8,8 +8,8 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class SettingsComponent implements OnInit {
 
-  public form: FormGroup;
-
+  public settingForm: FormGroup;
+  public globalConfig = globalConfig;
     /**
      * Constructor
      *
@@ -20,9 +20,17 @@ export class SettingsComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.form = this._formBuilder.group({
-     firstName : ['', Validators.required],
-     lastName  : ['', Validators.required],
- });
+ this.settingForm = this._formBuilder.group({
+  MerchantId:['', Validators.required],
+  MerchantUserName: ['', Validators.required],
+  FirstName: ['', Validators.required],
+  LastName: ['', Validators.required],
+  Address1: ['', Validators.required],
+  Address2: [''],
+  City: ['', Validators.required],
+  State: ['', Validators.required],
+  Zip: ['', [Validators.required, Validators.maxLength(globalConfig.validator.zipMaxLength)]],
+  Country: ['', Validators.required]
+});
  }
 }
