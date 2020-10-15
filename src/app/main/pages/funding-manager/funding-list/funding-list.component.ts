1099d615b3bcb446f2c80e9b9062d9aa644c91b1
muhammadsami985 <muhammadsami985@gmail.com>
2020-10-15 20:29:13 +0500

    changes of funded and non funded

--- src/app/main/pages/funding-manager/funding-list/funding-list.component.ts
+++ src/app/main/pages/funding-manager/funding-list/funding-list.component.ts
@@ -1,10 +1,13 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, ComponentFactory, ComponentFactoryResolver, ComponentRef, OnDestroy, OnInit, ViewChild, ViewContainerRef, ViewEncapsulation } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import { NoFoundComponent } from '@fuse/components/no-found/no-found.component';
 import { UserConfigService } from '@fuse/services/user.config.service';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { FundManagerService } from '../funding-manager.service';
+import { FundedTableComponent } from '../tabs/funded-table/funded-table.component';
+import { NonFundedTableComponent } from '../tabs/non-funded-table/non-funded-table.component';
 
 @Component({
   selector: 'app-funding-list',
@@ -14,9 +17,12 @@ import { FundManagerService } from '../funding-manager.service';
 
 })
 export class FundingListComponent implements OnInit, OnDestroy {
+  @ViewChild('renderingContainerFunded', { read: ViewContainerRef, static: false }) fundedContainer: ViewContainerRef;
+  private fundedComponentRef: ComponentRef<any>;
 
+  @ViewChild('renderingContainerNonFunded', { read: ViewContainerRef, static: false }) nonFundedContainer: ViewContainerRef;
+  private nonFundedComponentRef: ComponentRef<any>;
 
-  public displayedColumns = ['PartnerName', 'TotalTransAmount', 'PaynomixFee', 'AdminCommission', 'PartnerCommision', 'ResellerCommision', 'Status', 'Action'];
   public fundsList: any = [];
   private _unsubscribeAll: Subject<any>;
   
@@ -25,11 +31,14 @@ export class FundingListComponent implements OnInit, OnDestroy {
       *
       * @param {PartnerService} _fundManagerService
       * @param {UserConfigService} _userConfigService
+      * @param {ComponentFactoryResolver} _resolver
       */
      
      constructor(
        private readonly _fundManagerService: FundManagerService,
-       private readonly _userConfigService: UserConfigService
+       private readonly _userConfigService: UserConfigService,
+      private readonly _resolver: ComponentFactoryResolver
+
    ) { 
              // Set the private defaults
              this._unsubscribeAll = new Subject();
@@ -46,12 +55,36 @@ export class FundingListComponent implements OnInit, OnDestroy {
     this._unsubscribeAll.next();
     this._unsubscribeAll.complete();
   }
-
+  renderingComponent(type, data?) {
+    if(data.type == 'nonFunded'){
+      const factory: ComponentFactory<any> = this._resolver.resolveComponentFactory(type);
+      this.nonFundedContainer.clear();
+      this.nonFundedComponentRef = this.nonFundedContainer.createComponent(factory);
+      this.nonFundedComponentRef.instance.data = data;
+    }else{
+      const factory: ComponentFactory<any> = this._resolver.resolveComponentFactory(type);
+      this.fundedContainer.clear();
+      this.fundedComponentRef = this.fundedContainer.createComponent(factory);
+      this.fundedComponentRef.instance.data = data;
+    }
+  }
   getFundingList(): void{
     this._fundManagerService.fundingList(this._userConfigService.getUserMode())
     .then((res: any) => {
         if(res && !res.StatusCode){
+          if(res.Response && res.Response.length){
             this.fundsList = res.Response;
+            //to do
+            this.renderingComponent(NonFundedTableComponent, {
+              fundedList: this.fundsList,
+              type: 'nonFunded'
+            })
+            this.renderingComponent(FundedTableComponent, {
+              nonFundedList: this.fundsList,
+            })
+          }else{
+            this.renderingComponent(NoFoundComponent)
+          }
         }
     }).catch((err: HttpErrorResponse)=>(console.log))
   }
--- src/app/main/pages/funding-manager/tabs/funded-table/funded-table.component.ts
+++ src/app/main/pages/funding-manager/tabs/funded-table/funded-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
 
 @Component({
@@ -6,21 +6,21 @@ import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
   templateUrl: './funded-table.component.html',
   styleUrls: ['./funded-table.component.scss']
 })
-export class FundedTableComponent implements OnInit, OnChanges {
+export class FundedTableComponent implements OnInit {
   public dataSource = new MatTableDataSource<any>();
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   @ViewChild(MatSort, {static: true}) sort: MatSort;
-  @Input() fundedList: any;
-  @Input() displayedColumns: string[] = [];
+  @Input() data: any;
+  public displayedColumns: string[] = ['PartnerName', 'TotalTransAmount', 'PaynomixFee', 'AdminCommission', 'PartnerCommision', 'ResellerCommision', 'Status', 'Action'];
+  
   constructor() { }
 
-  ngOnInit(): void {}
-
-  ngOnChanges(): void{
-    if(this.fundedList.length){
-      this.dataSource.data = this.fundedList;
+  ngOnInit(): void {
+    if(this.data){
+      this.dataSource.data = this.data.fundedList;
       this.dataSource.paginator = this.paginator;
       this.dataSource.sort = this.sort;
     }
   }
+
 }
