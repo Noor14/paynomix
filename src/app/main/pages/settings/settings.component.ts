1566da9a5b1c9fc1b8a95ec93b653a7c8095cc9b
Noor14 <nooraliyahoo95@gmail.com>
2020-10-27 01:15:01 -0700

    basicInfo changes

--- src/app/main/pages/settings/settings.component.ts
+++ src/app/main/pages/settings/settings.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import { UserConfigService } from '@fuse/services/user.config.service';
+import { Subject } from 'rxjs/internal/Subject';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-settings',
@@ -6,10 +9,26 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./settings.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class SettingsComponent implements OnInit {
-   
-   constructor() {}
+export class SettingsComponent implements OnInit, OnDestroy {
+   public basicInfoVisibility: boolean = false;
+   private _unsubscribeAll: Subject<any>;
 
-   ngOnInit(): void {}
+   constructor(private readonly _userConfigService:  UserConfigService) {
+    this._unsubscribeAll = new Subject();
+   }
 
+   ngOnInit(): void {
+    this._userConfigService.userModeChange
+    .pipe(takeUntil(this._unsubscribeAll))
+    .subscribe((res) => {
+     this.basicInfoVisibility = res && !!Object.keys(res).length
+    });
+   }
+
+   ngOnDestroy(): void {
+    // Unsubscribe from all subscriptions
+    this._unsubscribeAll.next();
+    this._unsubscribeAll.complete();
+  }
 }
+
