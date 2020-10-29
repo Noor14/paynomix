3b1666bec2c7d3c42f3357cb14fc080537f498e8
muhammadsami985 <muhammadsami985@gmail.com>
2020-10-29 18:53:31 +0500

    filter implementation changes

--- src/@fuse/shared.module.ts
+++ src/@fuse/shared.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-
 import { FlexLayoutModule } from '@angular/flex-layout';
-
 import { FuseDirectivesModule } from '@fuse/directives/directives';
 import { FusePipesModule } from '@fuse/pipes/pipes.module';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { NgxMaskModule, IConfig } from 'ngx-mask';
 
+export const options: Partial<IConfig> | (() => Partial<IConfig>) = null;
 @NgModule({
     imports  : [
         CommonModule,
@@ -16,7 +16,8 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
         MatSnackBarModule,
         FlexLayoutModule,
         FuseDirectivesModule,
-        FusePipesModule
+        FusePipesModule,
+        NgxMaskModule.forRoot(),
     ],
     exports  : [
         CommonModule,
