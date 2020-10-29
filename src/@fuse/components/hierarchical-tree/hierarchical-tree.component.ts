f13303b3f14c8551f7051413414dbe6f7e34334f
Noor14 <nooraliyahoo95@gmail.com>
2020-10-29 03:39:44 -0700

    hirarchy changes

--- src/@fuse/components/hierarchical-tree/hierarchical-tree.component.ts
+++ src/@fuse/components/hierarchical-tree/hierarchical-tree.component.ts
@@ -1,13 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {NestedTreeControl} from '@angular/cdk/tree';
+import {MatTreeNestedDataSource} from '@angular/material/tree';
 
+interface FoodNode {
+  name: string;
+  children?: FoodNode[];
+}
+
+const TREE_DATA: FoodNode[] = [
+  {
+    name: 'Fruit',
+    children: [
+      {name: 'Apple'},
+      {name: 'Banana'},
+      {name: 'Fruit loops'},
+    ]
+  }, {
+    name: 'Vegetables',
+    children: [
+      {
+        name: 'Green',
+        children: [
+          {name: 'Broccoli'},
+          {name: 'Brussels sprouts'},
+        ]
+      }, {
+        name: 'Orange',
+        children: [
+          {name: 'Pumpkins'},
+          {name: 'Carrots'},
+        ]
+      },
+    ]
+  },
+];
 @Component({
   selector: 'app-hierarchical-tree',
   templateUrl: './hierarchical-tree.component.html',
-  styleUrls: ['./hierarchical-tree.component.scss']
+  styleUrls: ['./hierarchical-tree.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class HierarchicalTreeComponent implements OnInit {
 
-  constructor() { }
+  treeControl = new NestedTreeControl<FoodNode>(node => node.children);
+  dataSource = new MatTreeNestedDataSource<FoodNode>();
+
+  constructor() {
+    this.dataSource.data = TREE_DATA;
+  }
+
+  hasChild = (_: number, node: FoodNode) => !!node.children && node.children.length > 0;
 
   ngOnInit() {
   }
