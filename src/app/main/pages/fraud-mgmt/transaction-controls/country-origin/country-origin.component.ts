53f1f5e8a7e758bbad3a6823b7192c5a2964ea4e
Noor14 <nooraliyahoo95@gmail.com>
2020-11-04 15:38:30 +0500

    component created

---
+++ src/app/main/pages/fraud-mgmt/transaction-controls/country-origin/country-origin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-country-origin',
+  templateUrl: './country-origin.component.html',
+  styleUrls: ['./country-origin.component.scss']
+})
+export class CountryOriginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/main/pages/fraud-mgmt/transaction-controls/ip-blocking/ip-blocking.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ip-blocking',
+  templateUrl: './ip-blocking.component.html',
+  styleUrls: ['./ip-blocking.component.scss']
+})
+export class IpBlockingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
