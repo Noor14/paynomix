5b309b04857f171922856e398f84f23732012a4e
AffanAzeem7 <affan_azeem@hotmail.com>
2020-11-13 20:42:19 +0500

    refund work pending

--- src/app/main/pages/transaction/transaction.module.ts
+++ src/app/main/pages/transaction/transaction.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { TransactionRoutingModule } from './transaction-routing.module';
 import { TransactionListComponent } from './transaction-list/transaction-list.component';
 import { TransactionTableComponent } from './transaction-table/transaction-table.component';
-import { MatButtonModule, MatIconModule, MatInputModule, MatPaginatorModule, MatSortModule, MatTableModule } from '@angular/material';
+import { MatButtonModule, MatCheckboxModule, MatDatepickerModule, MatIconModule, MatInputModule, MatPaginatorModule, MatSortModule, MatTableModule, MatToolbarModule } from '@angular/material';
 import { FuseSharedModule } from '@fuse/shared.module';
 import { NoFoundModule } from '@fuse/components/no-found/no-found.module';
 import { TransactionDetailComponent } from './transaction-detail/transaction-detail.component';
@@ -16,6 +16,7 @@ import {MatTooltipModule} from '@angular/material/tooltip';
   ],
   exports: [TransactionTableComponent],
   imports: [
+    MatDatepickerModule,
     CommonModule,
     TransactionRoutingModule,
     NoFoundModule,
@@ -26,7 +27,9 @@ import {MatTooltipModule} from '@angular/material/tooltip';
     FuseSharedModule,
     MatIconModule,
     MatInputModule,
-    MatTooltipModule
+    MatTooltipModule,
+    MatCheckboxModule,
+    MatToolbarModule
   ]
 })
 export class TransactionModule { }
