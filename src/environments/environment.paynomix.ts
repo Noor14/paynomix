e2eff75c97a9b6ed2ce3d6e5bc6b53719a5be6bf
Noor14 <nooraliyahoo95@gmail.com>
2020-11-19 12:38:49 +0500

    changes

--- src/environments/environment.paynomix.ts
+++ src/environments/environment.paynomix.ts
@@ -3,8 +3,7 @@ export const environment = {
     hmr: false,
     mentom: false,
     paynomix : true,
-    //apiURL: 'https://api.paynomix.com:444/api/',
-    apiURL: 'https://paynomix.clique.center/api/api/',//Dev Environment
+    apiURL: 'https://api.paynomix.com:444/api/',
     title: 'Paynomix',
     assetPathName: 'paynomix',
     themeName: 'theme-default',
