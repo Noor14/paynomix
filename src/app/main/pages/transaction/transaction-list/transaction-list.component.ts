6227d89087ee602433f87e7068467d4fdbd26e14
prismpay.asim <prismpay.asim@gmail.com>
2020-12-01 17:02:33 +0500

    changes

--- src/app/main/pages/transaction/transaction-list/transaction-list.component.ts
+++ src/app/main/pages/transaction/transaction-list/transaction-list.component.ts
@@ -108,8 +108,8 @@ export class TransactionListComponent implements OnInit {
         } else {
           this.renderingComponent(NoFoundComponent, {
             icon: 'no-transaction',
-            text: 'No Transaction Found',
-            subText: "You Haven't made any Transaction"
+            text: 'No Transaction(s) Found',
+            subText: "You haven’t made any transaction"
           });
         }
       }
