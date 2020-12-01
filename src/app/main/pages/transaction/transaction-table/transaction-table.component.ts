2d60cb16252aae005140d37a2dc6d1d466e394bd
aliahmed3195 <aliahmed3195@gmail.com>
2020-12-01 13:42:58 +0500

    Transac.Refund-Expanded-Issue-Fixed

--- src/app/main/pages/transaction/transaction-table/transaction-table.component.ts
+++ src/app/main/pages/transaction/transaction-table/transaction-table.component.ts
@@ -23,7 +23,7 @@ import { Router } from '@angular/router';
   encapsulation: ViewEncapsulation.None,
   animations: [
     trigger('detailExpandRefund', [
-      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('collapsed', style({ height: 'auto', minHeight: 'auto' })),
       state('expanded', style({ height: '*' })),
       transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
     ]),
