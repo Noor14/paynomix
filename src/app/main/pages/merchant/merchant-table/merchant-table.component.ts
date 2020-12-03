07c966b10f75455803b4319f9cc0e6cf5963dc4e
prismpay.asim <prismpay.asim@gmail.com>
2020-12-03 17:42:18 +0500

    merchant pagination

--- src/app/main/pages/merchant/merchant-table/merchant-table.component.ts
+++ src/app/main/pages/merchant/merchant-table/merchant-table.component.ts
@@ -6,6 +6,8 @@ import { MatTableDataSource } from '@angular/material/table';
 import { fuseAnimations } from '@fuse/animations';
 import { EmailDialogComponent } from '@fuse/components/email-dialog/email-dialog.component';
 import { truncateTextLength } from '../../../../../constants/globalFunctions';
+import { UserConfigService } from '@fuse/services/user.config.service';
+import { MerchantService } from '../merchant.service';
 
 @Component({
   selector: 'app-merchant-table',
@@ -15,13 +17,19 @@ import { truncateTextLength } from '../../../../../constants/globalFunctions';
 })
 export class MerchantTableComponent implements OnInit {
   public truncateTextLength = truncateTextLength;
+  public recordCount: number = 0;
+
   public dataSource = new MatTableDataSource<any>();
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   @ViewChild(MatSort, {static: true}) sort: MatSort;
+ 
   public displayedColumns : string[] =  ['Reseller', 'CompanyName', 'ContactPerson', 'Email', 'PricingTitle', 'BoardedDate', 'BoardingStatus'];
   @Input() data: any;
   public actionControlOnHover = -1;
-  constructor(private readonly _dialog: MatDialog) { }
+  constructor(private readonly _dialog: MatDialog,
+    private readonly _userConfigService: UserConfigService,
+    private readonly _merchantService: MerchantService,) { }
+  
 
   ngOnInit(): void{
     if(this.data){
@@ -30,6 +38,11 @@ export class MerchantTableComponent implements OnInit {
       this.dataSource.sort = this.sort;
     }
   }
+
+  ngAfterViewInit(): void{
+    setTimeout(() =>  this.recordCount = 2000, 0);
+  }
+
   openDialog(obj) {
     const object:any = {
       SendTo: obj.MerchantAccountSetup.MerchantEmail,
@@ -41,4 +54,22 @@ export class MerchantTableComponent implements OnInit {
     const dialogRef = this._dialog.open(EmailDialogComponent, {width: '660px'});
     dialogRef.componentInstance.data = object;
   } 
+
+  changePage(event){ 
+    const obj = {
+      RecordLimit: event.pageSize,
+      PageNo: ++event.pageIndex,
+      ...this._userConfigService.getUserMode()
+    };
+    this._merchantService.merchantList(obj)
+    .then((res: any) => {
+      if (res && !res.StatusCode) {
+        if (res.Response ) {
+          this.dataSource = res.Response;
+        }
+      }
+    }).catch(() => (console.log))
+
+
+  }
 }
