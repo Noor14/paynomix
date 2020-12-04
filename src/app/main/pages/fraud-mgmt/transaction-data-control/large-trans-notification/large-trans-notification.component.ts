47d96a87a7183fa412f44328d7f3dc8f801a3765
prismpay.asim <prismpay.asim@gmail.com>
2020-12-04 18:36:48 +0500

    show error message when same partner and reseller name exists

--- src/app/main/pages/fraud-mgmt/transaction-data-control/large-trans-notification/large-trans-notification.component.ts
+++ src/app/main/pages/fraud-mgmt/transaction-data-control/large-trans-notification/large-trans-notification.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input,Output } from '@angular/core';
+import { UserConfigService } from '@fuse/services/user.config.service';
+import { TransactionDataControlsService } from '../transaction-data-controls.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { snackBarConfig,snackBarConfigWarn,validateAllFormFields } from 'constants/globalFunctions';
 
 @Component({
   selector: 'app-large-trans-notification',
@@ -6,10 +11,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./large-trans-notification.component.scss']
 })
 export class LargeTransNotificationComponent implements OnInit {
-
-  constructor() { }
+  public largeTransactionForm: FormGroup;
+  public disableForms: any;
+  constructor(
+    private readonly _formBuilder: FormBuilder,
+    private readonly _transactionDataControlsService: TransactionDataControlsService,
+    private readonly _userConfigService: UserConfigService,
+    private readonly _snackBar: MatSnackBar,
+    
+  ) { }
 
   ngOnInit(): void {
+    this.createlargeTransactionForm()
+  }
+
+  ngOnChanges(): void {
+    if(this.disableForms) {
+      this.largeTransactionForm.disable();
+    }
+  }
+
+  createlargeTransactionForm(): void {
+    this.largeTransactionForm = this._formBuilder.group({
+      TransactionAmount: [0, Validators.required],
+      Email: ['', Validators.required],
+      Status: [true, Validators.required]
+    })
+  }
+
+  addlargeNotification(): any {
+    if (this.largeTransactionForm.valid) {
+      const checkForUserRole = this._userConfigService.getUserMode();
+      const roleObject = (checkForUserRole) ? checkForUserRole : { EntityId: 0, UserRoleId: 1 }
+      const obj = {
+        ...this.largeTransactionForm.value,
+        ...roleObject
+      }
+      
+      this._transactionDataControlsService.addNotification(obj).then((res: any) => {
+        if (res && !res.StatusCode) {
+          this._snackBar.open(res.StatusMessage, '', snackBarConfig);
+          this.largeTransactionForm.reset(this.largeTransactionForm.value);
+        } else {
+          this._snackBar.open(res.StatusMessage, '', snackBarConfigWarn);
+        }
+      })
+
+
+    } else {
+      validateAllFormFields(this.largeTransactionForm)
+    }
   }
 
 }
